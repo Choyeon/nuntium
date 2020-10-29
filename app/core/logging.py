fa6f7007d9bd681ee1b87c521d25a94b39f022df
Choyeon <952223950@qq.com>
2020-10-29 08:25:41 +0800

    🌈提交脚手架

---
+++ app/core/logging.py
@@ -0,0 +1,10 @@
+import logging
+
+from loguru import logger
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:  # pragma: no cover
+        logger_opt = logger.opt(depth=7, exception=record.exc_info)
+        logger_opt.log(record.levelname, record.getMessage())
+        # todo 日志写到文件和数据库
---
+++ app/models/prediction.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class MachineLearningResponse(BaseModel):
+    prediction: float
+
+
+class HealthResponse(BaseModel):
+    status: bool
